ac03af77cbb7260700cbc0af944dffe630950232
Christoffer G. Thomsen <chris@cgt.name>
2018-01-30 13:26:26 +0100

    Test FlatWhite

---
+++ kaffeTests/FlatWhiteTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using kaffe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kaffeTests
+{
+    [TestClass]
+    public class FlatWhiteTests
+    {
+        [TestMethod]
+        public void TestStyrke()
+        {
+            var fw = new FlatWhite();
+            Assert.AreEqual("Mild", fw.Styrke());
+        }
+
+        [TestMethod]
+        public void TestPris()
+        {
+            var fw = new FlatWhite();
+            Assert.AreEqual(45, fw.Pris());
+        }
+
+        [TestMethod]
+        public void TestMlMælk()
+        {
+            var fw = new FlatWhite();
+            Assert.AreEqual(160, fw.MlMælk());
+        }
+    }
+}
